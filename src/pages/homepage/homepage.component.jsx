c5fa16a768e2c4503b3b5737c599d5a1ba89bd6b
unknown <irvinloc@hotmail.com>
2020-06-06 12:00:36 +0200

    mogollon

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+//import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
+
+
+const HomePage = () => (
+    <div className="homepage" >
+      <Directory ></Directory>
+           
+    </div>
+);
+
+
+export default HomePage;
